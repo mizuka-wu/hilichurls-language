268298a708811d0766c5bb13d1a0a927d99fa137
水华 <mizuka.wu@outlook.com>
2021-05-06 22:49:03 +0800

    feat: 分割句子算法改进

--- src/App.vue
+++ src/App.vue
@@ -132,11 +132,12 @@ export default {
     },
     result ({ source, arrayTypeDirectory, md5Directory }) {
       const segments = Array.from(source
-        .matchAll(/(?<text>[\w\s]*)(?<symbol>[\n,!.\s\\n]*)/g))
+        .matchAll(/(?<text>[\w\x20]*)(?<symbol>[\n,!.\s(\\n)]+)/g))
         .map(item => item.groups)
         .filter(item => item.segment !== '' || item.symbol !== '')
+
       return segments.map(({ text, symbol }) => ({
-        symbol: symbol.replace(/\n\\n/g, '<br/>'),
+        symbol: symbol.replace(/[\n,\\n]/g, '<br/>'),
         text: arrayTypeDirectory
           .reduce((_text, dir) => _text.replace(dir.text, ` ${dir.md5} `), text)
           .replace(/\s+/g, ' ')
